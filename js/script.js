97fa15eae6c8e448efae607341905b66b05e4263
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-24 14:36:50 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+//This generates hello world
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit6-02-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-02-HTML/",
+  })
+}
+
+function cookies() {
+  if (localStorage.hits) {
+    localStorage.hits++
+  } else {
+    localStorage.hits = 1
+  }
+
+  document.getElementById("total").innerHTML =
+    "You have clicked " + localStorage.hits + "!"
+}
